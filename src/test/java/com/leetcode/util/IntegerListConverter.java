15c436cce04031714622db6502f87ca92ef061b4
olegmashkov <olegmashkov21@gmail.com>
2023-01-09 10:11:13 +0300

    Implement IntegerListConverter.class

---
+++ src/test/java/com/leetcode/util/IntegerListConverter.java
@@ -0,0 +1,24 @@
+package com.leetcode.util;
+
+import org.junit.jupiter.params.converter.ArgumentConversionException;
+import org.junit.jupiter.params.converter.SimpleArgumentConverter;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class IntegerListConverter extends SimpleArgumentConverter {
+    @Override
+    protected Object convert(Object source, Class<?> targetType) throws ArgumentConversionException {
+        if (source instanceof String && List.class.isAssignableFrom(targetType)) {
+            String[] split = ((String) source).split("\\s*,\\s*");
+            List<Integer> result = new ArrayList<>();
+            for (String s : split) {
+                result.add(Integer.parseInt(s));
+            }
+            return result;
+        } else {
+            throw new IllegalArgumentException(
+                    String.format("Conversion from %s to %s not supported.", source.getClass(), targetType));
+        }
+    }
+}
